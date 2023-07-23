bee9dbed49dbb4867700efa2be3265e479a59d09
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-07-23 22:57:16 +0530

    Update Find_Maximum_Number.java

--- Find_Maximum_Number.java
+++ Find_Maximum_Number.java
@@ -1,10 +1,4 @@
-// Given a number N, write a program to find a maximum number that can be formed using all of the digits of this number.
-// Example 1:
-// Input:
-// N = "38293367"
-// Output:
-// 98763332 
-// Explanation: 98763332 is the largest number that can be formed by rearranging the digits of N.
+
 
 class Solution {
     static String findMax(String N) {
